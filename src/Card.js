c384e597e9a53d8243b35cb9f54911ae89bedd5f
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-10 23:41:13 -0600

    decontruct prop

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = (props) => {
-    return (
-        <div className='tc bg-light-green dib br3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${props.id}`} alt="robots"/>
-            <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
-            </div>
-        </div>
-    )
-}
+const Card = ({ name, email, id }) => {
+  return (
+    <div className="tc bg-light-blue dib br3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}`} alt="robots" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
