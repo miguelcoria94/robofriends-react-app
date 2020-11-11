5c36b9227e9824cd660973ff173d46238cc5ba07
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-10 22:49:58 -0600

    created first component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1> Hello </h1>
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
