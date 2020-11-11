a5a6e7fc8204505f8320b69ae3e3fe5d913b1f74
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-11 13:58:16 -0600

    fix filename typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
-import SearchBox from './SearchBoc'
+import SearchBox from './SearchBox'
 
 const App = () => {
     return (
---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div className="pa2">
+            <input></input>
+        </div>
+    )
+}
+
+export default SearchBox;
