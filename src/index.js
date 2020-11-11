8fa95c875bb7dfc886d4aa48d2da310b7ea071a0
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-10 23:22:41 -0600

    start card component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello.js';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card.js';
 
 ReactDOM.render(
-    <Hello greeting={"Hello React Ninja"} />,
+  <Card />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
